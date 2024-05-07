3cfad09c4a9d3a0d4c2515a0bbd9d9f4916c8688
Amisha20223 <amisha.2125ec1132@kiet.edu>
2024-05-08 01:05:20 +0530

    change one

--- config/languages.ts
+++ config/languages.ts
@@ -1,48 +1,41 @@
 export const languages = [
   {
    
-    title: 'Spanish',
-    flag: 'es',
+  
     word: 'ChatBot',
   },
   {
-    title: 'Spanish',
-    flag: 'es',
-    word: 'ChatBot',
+    
+    word: 'Budget',
   },
   {
-    title: 'French',
-    flag: 'fr',
+    
     word: 'Quizzes',
   },
   {
-    title: 'Korean',
-    flag: 'kr',
+    
+    
     word: 'Feedback',
   },
   {
-    title: 'Japanese',
-    flag: 'jp',
+   
     word: 'Stocks',
   },
   {
-    title: 'Mandarin',
-    flag: 'cn',
+   
     word: 'Investment',
   },
   {
-    title: 'Italian',
-    flag: 'it',
+   
     word: 'Marketing',
   },
   {
-    title: 'Russian',
-    flag: 'ru',
+    
     word: 'Savings',
   },
   {
-    title: 'German',
-    flag: 'de',
+    
+    
     word: 'Trading',
   },
 ]
