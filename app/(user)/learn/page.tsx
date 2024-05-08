65dffff0aa6323be5528087523d618b1d5c53069
Surabhi <surabhi.2125cs1188@kiet.edu>
2024-05-09 01:58:17 +0530

    fin

--- app/(user)/learn/page.tsx
+++ app/(user)/learn/page.tsx
@@ -63,12 +63,12 @@ export function SideMenu() {
         </NextLink>
         <div className="flex flex-col w-full h-screen justify-center items-center gap-y-10">
           <div className='flex flex-row gap-x-10'>
-            <SideMenuInnerItem href="/learn"  modulename = "budget" icon="learn" label="Learning Modules" />
-            <SideMenuInnerItem href="/leaderboard"  modulename = "budget" icon="leaderboard" label="Quizzes" />
+            <SideMenuInnerItem href="/learn"  modulename = "budget" icon="learn"  label="Blogs/Articles" />
+            <SideMenuInnerItem href="/leaderboard"  modulename = "budget" icon="leaderboard" label="Gamiefied Learning" />
           </div>
           <div className='flex flex-row gap-x-10'>
-            <SideMenuInnerItem href="/chatboat" modulename = "budget" icon="quests" label="Chatbot" />
-            <SideMenuInnerItem href="/shop" icon="shop"  modulename = "budget" label="Feedback" />
+            <SideMenuInnerItem href="/chatboat" modulename = "budget" icon="quests" label="Videos" />
+            <SideMenuInnerItem href="/shop" icon="shop"  modulename = "budget" label="Progress Tracker" />
           </div>
         </div>
       </nav>
