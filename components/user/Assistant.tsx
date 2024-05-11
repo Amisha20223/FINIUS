8e8e89738ac4117830050394e2bbc3cbd4ad6998
Amisha20223 <amisha.2125ec1132@kiet.edu>
2024-05-11 11:54:26 +0530

    done

--- components/user/Assistant.tsx
+++ components/user/Assistant.tsx
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-=======
 // assistant.tsx
 import React from 'react';
 import NextLink from 'next/link';
 
->>>>>>> e49c2fa153b21bc2525110df8f1c7200aa84303e
 export function Assistant() {
   // Component logic here
 //   return <div>Assistant component</div>;
   return (
     <div className="relative">
       {/* Other content of your assistant page */}
-<<<<<<< HEAD
-      <img
-  src="/robot.png" 
-  alt="Any DOUBT? ASK ME"
-  className="absolute bottom-40 right-10 w-50 animate-bounce"
-/>
-
-    </div>
-  );
-}
-=======
       {/* <img
         src="/robot.png"
         alt="Doubts? Ask me!"
@@ -58,7 +44,6 @@ export function Assistant() {
 
 
 
->>>>>>> e49c2fa153b21bc2525110df8f1c7200aa84303e
 // AssistantPage.jsx
 // import React from 'react';
 
