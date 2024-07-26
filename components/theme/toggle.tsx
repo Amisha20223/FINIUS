38f810dbcc7b939ddea83104e898d3e6a22a90c3
Amisha20223 <120022430+Amisha20223@users.noreply.github.com>
2024-07-26 16:34:22 +0530

    Update toggle.tsx

--- components/theme/toggle.tsx
+++ components/theme/toggle.tsx
@@ -11,7 +11,7 @@ export function ThemeToggle({ className, ...props }: ButtonProps) {
 
   // TODO: fix layout shift from hydration
   if (!hydrated) return null
-
+ 
   return (
     <Button
       variant="ghost"
